8a7cda48cd9dad5114f99e0fb0f05ad61ccde140
Phil Holden <phil.holden@outlook.com.au>
2021-04-11 18:28:16 +1000

    fix: choco install

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,9 +1,11 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 
 function Install-Chocolatey {
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    if (-Not $env:ChocolateyInstall) {
+        Set-ExecutionPolicy Bypass -Scope Process -Force;
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
 }
 
 function Install-FromChocolatey {
@@ -29,7 +31,6 @@ Install-FromChocolatey 'greenshot'
 Install-FromChocolatey 'notepadplusplus'
 Install-FromChocolatey 'zoomit'
 Install-FromChocolatey 'powershell-core'
-Install-FromChocolatey ''
 
 # - dev tools
 Install-FromChocolatey 'azure-cli'
