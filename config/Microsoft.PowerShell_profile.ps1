9fd2cdbc28215240713fdf936af50a2a1ffa11c8
phil-holden <60332957+phil-holden@users.noreply.github.com>
2021-04-11 21:48:18 +1000

    feat: create powershell prpofile

---
+++ config/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Import-Module -Name posh-git
+Import-Module -Name oh-my-posh
+Set-Theme Pure
+
+Import-Module -Name Az.Tools.Predictor
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
