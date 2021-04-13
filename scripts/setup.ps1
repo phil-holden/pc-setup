edd9b30b5b751e8f7223b4b1d4644847bbe68696
phil-holden <60332957+phil-holden@users.noreply.github.com>
2021-04-13 10:34:32 +1000

    fix: refreshenv after choco install

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -3,6 +3,8 @@ function Install-Chocolatey {
         Set-ExecutionPolicy Bypass -Scope Process -Force;
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        
+        refreshenv
     }
 }
 
