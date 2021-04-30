c402449688ef577ae8b4807c20de021ee2a42f41
Phil Holden <>
2021-04-30 16:49:04 +1000

    feat: lab3 tweaks

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 Import-Module -Name posh-git
 Import-Module -Name oh-my-posh
-Set-Theme Pure
+Set-PoshPrompt -Theme ~/.poshthemes/.agnostercustom.omp.json
 
 Import-Module -Name Az.Tools.Predictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
@@ -8,3 +8,6 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 
 function Run-Terraform { & terraform $args }
 New-Alias -Name tf -Value Run-Terraform -Force -Option AllScope
+
+function Get-MyIp { $(Invoke-WebRequest "https://icanhazip.com").Content }
+New-Alias -Name myip -Value Get-MyIp -Force -Option AllScope
