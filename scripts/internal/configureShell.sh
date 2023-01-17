f0b7d7a8a61036db9e36de023b605c60dff03ad4
Phil Holden <phil.holden@aff.gov.au>
2023-01-17 16:50:10 +1100

    fix(wsl): zshrc setup

--- scripts/internal/configureShell.sh
+++ scripts/internal/configureShell.sh
@@ -8,9 +8,7 @@ if [[ ! -d ".oh-my-zsh" ]]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-if [[ ! -f ".zshrc" ]]; then
-    curl https://raw.githubusercontent.com/phil-holden/pc-setup/main/config/.zshrc --silent >  .zshrc
-fi
+curl https://raw.githubusercontent.com/phil-holden/pc-setup/main/config/.zshrc --silent >  .zshrc
 
 if [[ ! -f ".oh-my-post/custom/themes/agnoster.zsh-theme" ]]; then
     curl https://raw.githubusercontent.com/phil-holden/pc-setup/main/config/agnoster.zsh-theme --silent > .oh-my-zsh/custom/themes/agnoster.zsh-theme
